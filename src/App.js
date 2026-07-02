67cd03777ce291189e6f9d586bf1de60ef7450d0
Gonçalo Alegria <296128248+GOMAA0001@users.noreply.github.com>
2026-07-02 14:12:15 +0100

    ?

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-neutral-950 text-base flex relative justify-center items-center w-screen h-screen">Hello world!</h1>
+    <h1 className="text-neutral-950 text-base flex relative justify-center items-center w-screen h-screen">Hello world!!!</h1>
   );
 }
 
