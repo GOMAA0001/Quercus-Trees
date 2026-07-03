d52f7dee278032b938e2a18ff457449a36e3bde7
Gonçalo Alegria <296128248+GOMAA0001@users.noreply.github.com>
2026-07-03 01:08:34 +0100

    small change

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div id='Body'>
       <div className=' grid relative bg-emerald-100 w-full min-h-screen'>
-        <h1 className=' text-4xl'>hiii</h1>
       </div>
     </div>
   );
