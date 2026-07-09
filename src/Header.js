a6638028afeaaa579ca471f1d37f1d01fdc2a0e5
Gonçalo Alegria <296128248+GOMAA0001@users.noreply.github.com>
2026-07-09 23:40:42 +0100

    just normal changes

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,7 @@ import './App.css';
 function Header() {
   return (
     <div>
-      <div className=' w-1/4 h-32 bg-green-400 flex float-right'></div>
-      <div className=" w-3/4 h-32 bg-green-100 flex"></div>
+      <div className=" w-full h-36 bg-teal-400 flex"></div>
     </div>
   );
 }
