63ecd81f52dd9cf34e5501c003dbc71a6bba1aaa
Gonçalo Alegria <296128248+GOMAA0001@users.noreply.github.com>
2026-07-13 16:33:43 +0100

    more pretty

--- src/App.js
+++ src/App.js
@@ -6,27 +6,27 @@ function App() {
   <body className=" bg-stone-100 font-sans">
     <div className="grid max-w-4xl mx-auto p-8">
       <a href='https://github.com/GOMAA0001' target='_blank' className='bg-white rounded h-full text-gray-950 no-underline shadow-md'>
-        <h1 className=' text-2xl p-8'>Quercus robur</h1>
+        <h1 className=' text-2xl p-8 text-center italic'>Quercus robur</h1>
         <img className='w-full block rounded-b' src={require("./img/placeholder.png")} alt="Quercus robur"></img>
       </a>
       <a href='https://github.com/GOMAA0001' target='_blank' className='bg-white rounded h-full text-gray-950 no-underline shadow-md'>
-        <h1 className=' text-2xl p-8'>Quercus pyrenaica</h1>
+        <h1 className=' text-2xl p-8 text-center italic'>Quercus pyrenaica</h1>
         <img className='w-full block rounded-b' src={require("./img/placeholder.png")} alt="Quercus pyrenaica"></img>
       </a>
       <a href='https://github.com/GOMAA0001' target='_blank' className='bg-white rounded h-full text-gray-950 no-underline shadow-md'>
-        <h1 className=' text-2xl p-8'>Quercus faginea</h1>
+        <h1 className=' text-2xl p-8 text-center italic'>Quercus faginea</h1>
         <img className='w-full block rounded-b' src={require("./img/placeholder.png")} alt="Quercus faginea"></img>
       </a>
       <a href='https://github.com/GOMAA0001' target='_blank' className='bg-white rounded h-full text-gray-950 no-underline shadow-md'>
-        <h1 className=' text-2xl p-8'>Quercus ilex</h1>
+        <h1 className=' text-2xl p-8 text-center italic'>Quercus ilex</h1>
         <img className='w-full block rounded-b' src={require("./img/placeholder.png")} alt="Quercus ilex"></img>
       </a>
       <a href='https://github.com/GOMAA0001' target='_blank' className='bg-white rounded h-full text-gray-950 no-underline shadow-md'>
-        <h1 className=' text-2xl p-8'>Quercus suber</h1>
+        <h1 className=' text-2xl p-8 text-center italic'>Quercus suber</h1>
         <img className='w-full block rounded-b' src={require("./img/placeholder.png")} alt="Quercus suber"></img>
       </a>
       <a href='https://github.com/GOMAA0001' target='_blank' className='bg-white rounded h-full text-gray-950 no-underline shadow-md'>
-        <h1 className=' text-2xl p-8'>Quercus coccifera</h1>
+        <h1 className=' text-2xl p-8 text-center italic'>Quercus coccifera</h1>
         <img className='w-full block rounded-b' src={require("./img/placeholder.png")} alt="Quercus coccifera"></img>
       </a>
     </div>
